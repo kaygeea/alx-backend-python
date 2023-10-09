624d3c308facf0da93e548a6d9feb320833a6396
Koyejo <adinlewakoyejo@yahoo.com>
2023-10-09 10:24:50 +0000

    Add typ-annotated func to return str rep of float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Define function that returns the string representation of a float"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to string and return the string.
+
+    Args:
+        n (float): The floating-point number to be converted.
+
+    Returns:
+        Str: A string representation of the floating-point number.
+    """
+    return str(n)
