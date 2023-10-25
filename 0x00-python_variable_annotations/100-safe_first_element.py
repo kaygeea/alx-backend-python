5aa568178446e4c4e6ad3d9cef3adab09f67d6f7
Koyejo <adinlewakoyejo@yahoo.com>
2023-10-25 14:33:56 +0000

    Add func annotation for func with unknown input type

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Add type annotation for function with params of unknown type"""
+from typing import Any, Sequence, Union
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    Retrieve the first (i.e 0th) element of a sequence
+
+    Arg(s):
+        lst - Incoming argument for a sequence of vars of any type
+
+    Return:
+        The element at the 0th index of lst or None if lst is empty
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
