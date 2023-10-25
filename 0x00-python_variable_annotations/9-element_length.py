99906bab11b5adc35abc0a652f91651307f77f2a
Koyejo <adinlewakoyejo@yahoo.com>
2023-10-25 11:11:40 +0000

    Annotate function with proper types

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+#/usr/bin/env python3
+"""Annotate a func params and return values"""
+from typing import Iterable, List, Sequence, Tuple
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Annotated function
+    """
+    return [(i, len(i)) for i in lst]
